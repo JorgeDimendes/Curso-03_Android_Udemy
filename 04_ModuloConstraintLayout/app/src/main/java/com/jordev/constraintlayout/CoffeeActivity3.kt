ddfa2a173b12ddacc67d8dfcd7631a07d829d6a8
Jorge <jorgedimendes@hotmail.com>
2023-10-16 08:41:30 -0300

    Iniciando App Convidados

---
+++ 04_ModuloConstraintLayout/app/src/main/java/com/jordev/constraintlayout/CoffeeActivity3.kt
@@ -0,0 +1,11 @@
+package com.jordev.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee3)
+    }
+}
